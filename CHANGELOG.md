0be96e27ab6d84b206524b072e31b7a2c4b57ce3
Richard Fussenegger <Fleshgrinder@users.noreply.github.com>
2017-03-16 22:21:18 +0100

    New string modifiers and performance improvement (#1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,21 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/) and this
 
 ## [Unreleased][unreleased]
 
+## [1.1.0] - 2017-03-16
+### Added
+- `#c` string format modifier to transform ASCII control characters to their
+  [caret notation](https://en.wikipedia.org/wiki/Caret_notation) in the
+  formatted string.
+- `#p` string format modifier to transform ASCII control characters to their
+  printable Unicode pendant in the formatted string.
+- `strpos` check for brackets to avoid the heavy recursive regular expression
+  for optional parts if it is definitely not needed.
+### Changed
+- Empty strings are now formatted as `empty {:?}` in the formatted string
+  instead of resulting in nothing.
+
 ## 1.0.0 - 2017-03-12
 - Initial Release
 
-[unreleased]: https://github.com/Fleshgrinder/php-pattern-formatter/compare/1.0.0...HEAD
+[unreleased]: https://github.com/Fleshgrinder/php-pattern-formatter/compare/1.1.0...HEAD
+[1.1.0]: https://github.com/Fleshgrinder/php-pattern-formatter/compare/1.0.0...1.1.0
--- tests/FormatterTest.php
+++ tests/FormatterTest.php
@@ -66,86 +66,86 @@ final class FormatterTest extends TestCase {
 
 			'a bool true argument.' => [
 				'TRUE',
-			    '{}',
-			    [\true],
+				'{}',
+				[\true],
 			],
 
 			'a bool false argument.' => [
 				'FALSE',
-			    '{}',
-			    [\false],
+				'{}',
+				[\false],
 			],
 
 			'a stream resource argument.' => [
 				'stream resource',
-			    '{:?}',
-			    [\tmpfile()],
+				'{:?}',
+				[\tmpfile()],
 			],
 
 			'a closed resource argument.' => [
 				Value::TYPE_CLOSED_RESOURCE,
-			    '{:?}',
-			    [$closed_resource],
+				'{:?}',
+				[$closed_resource],
 			],
 
 			'a float argument.' => [
 				'0',
-			    '{}',
-			    [0.123456789],
+				'{}',
+				[0.123456789],
 			],
 
 			'a float argument and decimal modifier.' => [
 				'0.12346',
-			    '{.5}',
-			    [0.123456789],
+				'{.5}',
+				[0.123456789],
 			],
 
 			'a float argument and exponent notation modifier.' => [
 				'1.234568e-1',
-			    '{#e}',
-			    [0.123456789],
+				'{#e}',
+				[0.123456789],
 			],
 
 			'a named float argument and decimal modifier.' => [
 				'0.12346',
-			    '{float.5}',
-			    ['float' => 0.123456789],
+				'{float.5}',
+				['float' => 0.123456789],
 			],
 
 			'a fixed positional float argument and decimal modifier.' => [
 				'0.12346',
-			    '{42.5}',
-			    [42 => 0.123456789],
+				'{42.5}',
+				[42 => 0.123456789],
 			],
 
 			'an integer argument.' => [
 				'1',
-			    '{}',
-			    [1],
+				'{}',
+				[1],
 			],
 
 			'an integer argument and decimal modifier.' => [
 				'1.0',
-			    '{.1}',
-			    [1],
+				'{.1}',
+				[1],
 			],
 
 			'an integer argument and binary modifier.' => [
 				'0b10',
-			    '{#b}',
-			    [2],
+				'{#b}',
+				[2],
 			],
 
 			'an integer argument and octal modifier.' => [
 				'0o755',
-			    '{#o}',
-			    [493],
+				'{#o}',
+				[493],
 			],
 
 			'an integer argument and hexadecimal modifier.' => [
 				'0x2A',
-			    '{#x}',
-			    [42],
+				'{#x}',
+				[42],
 			],
 
 			'an empty array argument.' => [
@@ -160,92 +160,92 @@ final class FormatterTest extends TestCase {
 				[['k1' => 'one']],
 			],
 
-		    'an array argument and two values.' => [
-		    	'Hello, World',
-		        '{}',
-		        [['Hello', 'World']],
-		    ],
+			'an array argument and two values.' => [
+				'Hello, World',
+				'{}',
+				[['Hello', 'World']],
+			],
 
-		    'an array argument, two values, and a conjunction.' => [
-		    	'one and two',
-		        '{:and}',
-		        [['one', 'two']],
-		    ],
+			'an array argument, two values, and a conjunction.' => [
+				'one and two',
+				'{:and}',
+				[['one', 'two']],
+			],
 
-		    'an array argument and three values.' => [
-		    	'one, two, three',
-		        '{}',
-		        [['one', 'two', 'three']],
-		    ],
+			'an array argument and three values.' => [
+				'one, two, three',
+				'{}',
+				[['one', 'two', 'three']],
+			],
 
-		    'an array argument, four values, and a conjunction.' => [
-		    	'one, two, three, and four',
-		        '{:and}',
-		        [['one', 'two', 'three', 'four']],
-		    ],
+			'an array argument, four values, and a conjunction.' => [
+				'one, two, three, and four',
+				'{:and}',
+				[['one', 'two', 'three', 'four']],
+			],
 
-		    'an array argument, four values, and an escape closing brace conjunction.' => [
-		    	'one, two, three, } four',
-		        '{:}}}',
-		        [['one', 'two', 'three', 'four']],
-		    ],
+			'an array argument, four values, and an escape closing brace conjunction.' => [
+				'one, two, three, } four',
+				'{:}}}',
+				[['one', 'two', 'three', 'four']],
+			],
 
-		    'an object with a `__toString` method.' => [
-		    	'message',
-		        '{}',
-		        [new class {
+			'an object with a `__toString` method.' => [
+				'message',
+				'{}',
+				[new class {
 					public function __toString() {
 						return 'message';
 					}
-		        }],
-		    ],
+				}],
+			],
 
-		    'an object with a `toString` method.' => [
-		    	'message',
-		        '{}',
-		        [new class {
+			'an object with a `toString` method.' => [
+				'message',
+				'{}',
+				[new class {
 					public function toString(): string {
 						return 'message';
 					}
-		        }],
-		    ],
+				}],
+			],
 
-		    'an object with a `toInt` method, and a decimal modifier.' => [
-		    	'1,000.00',
-		        '{.2}',
-		        [new class {
+			'an object with a `toInt` method, and a decimal modifier.' => [
+				'1,000.00',
+				'{.2}',
+				[new class {
 					public function toInt(): int {
 						return 1000;
 					}
-		        }],
-		    ],
+				}],
+			],
 
-		    'an object with a `toFloat` method, and a decimal modifier.' => [
-		    	'0.12346',
-		        '{.5}',
-		        [new class {
+			'an object with a `toFloat` method, and a decimal modifier.' => [
+				'0.12346',
+				'{.5}',
+				[new class {
 					public function toFloat(): float {
 						return 0.123456789;
 					}
-		        }],
-		    ],
+				}],
+			],
 
-		    'optional part that is not included.' => [
-		    	'always',
-		        'always[ {}?]',
-		        [''],
-		    ],
+			'optional part that is not included.' => [
+				'always',
+				'always[ {}?]',
+				[''],
+			],
 
-		    'optional part that is included.' => [
-		    	'always optional',
-		        'always[ {0}?]',
-		        ['optional'],
-		    ],
+			'optional part that is included.' => [
+				'always optional',
+				'always[ {0}?]',
+				['optional'],
+			],
 
 			'optional part that is include, with a single marked placeholder.' => [
 				'always optional suffix',
-			    'always[ {0}? {1}]',
-			    ['optional', 'suffix'],
+				'always[ {0}? {1}]',
+				['optional', 'suffix'],
 			],
 
 			'optional part that is not included, with multiple marked placeholders where all are empty.' => [
@@ -260,17 +260,55 @@ final class FormatterTest extends TestCase {
 				['foo', 'bar', ''],
 			],
 
-		    'two positional placeholders, and escaped brackets.' => [
-		    	'[0.0, 1.0]',
-		        '[[{.1}, {.1}]]',
-		        [0, 1],
-		    ],
+			'two positional placeholders, and escaped brackets.' => [
+				'[0.0, 1.0]',
+				'[[{.1}, {.1}]]',
+				[0, 1],
+			],
 
-		    'various placeholders, and escaped brackets and braces.' => [
-		    	'{prefix} value [suffix]',
-		        '{{prefix}} {} [[suffix]]',
-		        ['value'],
-		    ],
+			'various placeholders, and escaped brackets and braces.' => [
+				'{prefix} value [suffix]',
+				'{{prefix}} {} [[suffix]]',
+				['value'],
+			],
+
+			'empty string argument.' => [
+				'empty string',
+				'{}',
+				[''],
+			],
+
+			'void argument.' => [
+				'void',
+				'{:?}',
+				[],
+			],
+
+			'caret notation for all ASCII control characters.' => [
+				'^@, ^A, ^B, ^C, ^D, ^E, ^F, ^G, ^H, ^I, ^J, ^K, ^L, ^M, ^N, ^O, ^P, ^Q, ^R, ^S, ^T, ^U, ^V, ^W, ^X, ^Y, ^Z, ^[, ^\, ^], ^^, ^_,  , ~, ^?',
+				'{#c}',
+				[[
+					"\u{00}", "\u{01}", "\u{02}", "\u{03}", "\u{04}", "\u{05}",
+					"\u{06}", "\u{07}", "\u{08}", "\u{09}", "\u{0A}", "\u{0B}",
+					"\u{0C}", "\u{0D}", "\u{0E}", "\u{0F}", "\u{10}", "\u{11}",
+					"\u{12}", "\u{13}", "\u{14}", "\u{15}", "\u{16}", "\u{17}",
+					"\u{18}", "\u{19}", "\u{1A}", "\u{1B}", "\u{1C}", "\u{1D}",
+					"\u{1E}", "\u{1F}", ' ', '~', "\u{7F}",
+				]]
+			],
+
+			'Unicode characters for all ASCII control characters.' => [
+				'␀, ␁, ␂, ␃, ␄, ␅, ␆, ␇, ␈, ␉, ␊, ␋, ␌, ␍, ␎, ␏, ␐, ␑, ␒, ␓, ␔, ␕, ␖, ␗, ␘, ␙, ␚, ␛, ␜, ␝, ␞, ␟,  , ~, ␡',
+				'{#p}',
+				[[
+					 "\u{00}", "\u{01}", "\u{02}", "\u{03}", "\u{04}", "\u{05}",
+					 "\u{06}", "\u{07}", "\u{08}", "\u{09}", "\u{0A}", "\u{0B}",
+					 "\u{0C}", "\u{0D}", "\u{0E}", "\u{0F}", "\u{10}", "\u{11}",
+					 "\u{12}", "\u{13}", "\u{14}", "\u{15}", "\u{16}", "\u{17}",
+					 "\u{18}", "\u{19}", "\u{1A}", "\u{1B}", "\u{1C}", "\u{1D}",
+					 "\u{1E}", "\u{1F}", ' ', '~', "\u{7F}",
+				 ]]
+			],
 		];
 	}
 
@@ -279,6 +317,9 @@ final class FormatterTest extends TestCase {
 	 * @covers \Fleshgrinder\Core\Formatter::format
 	 * @covers \Fleshgrinder\Core\Formatter::formatArg
 	 * @covers \Fleshgrinder\Core\Formatter::formatArrayArg
+	 * @covers \Fleshgrinder\Core\Formatter::formatNumber
+	 * @covers \Fleshgrinder\Core\Formatter::formatString
+	 * @covers \Fleshgrinder\Core\Formatter::formatControlChars
 	 * @dataProvider providePatterns
 	 */
 	public static function testFormat(string $expected, string $pattern, array $arguments) {
@@ -302,6 +343,8 @@ final class FormatterTest extends TestCase {
 	 * @testdox Format throws a `MissingPlaceholderException` if a placeholder is missing from the arguments.
 	 * @covers \Fleshgrinder\Core\Formatter::format
 	 * @covers \Fleshgrinder\Core\Formatter::formatArg
+	 * @covers \Fleshgrinder\Core\Formatter::formatNumber
+	 * @covers \Fleshgrinder\Core\Formatter::formatString
 	 * @uses \Fleshgrinder\Core\Formatter\MissingPlaceholderException
 	 * @expectedException \Fleshgrinder\Core\Formatter\MissingPlaceholderException
 	 * @expectedExceptionMessage Placeholder `0` not found in arguments, the following placeholders were present: none
